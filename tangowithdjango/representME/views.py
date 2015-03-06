1d82edd517fcdecbc7b97be4d5f2f319267b2dfc
Emil Cieslar <emil.cieslar@gmail.com>
2015-03-06 17:46:23 +0000

    Basic setup of urls, views and templates

--- tangowithdjango/representME/views.py
+++ tangowithdjango/representME/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from representME.models import Law
 
 # Create your views here.
+def index(request):
+    context_dict = {'bold_message': "I am bold message"}
+
+    return render(request,'representME/index.html', context_dict)
+
+def law(request, law_name_slug):
+    context_dict = {}
+
+    try:
+        # instead of name it should be slug, but our db doesn't have a slug yet
+        law = Law.objects.get(name=law_name_slug)
+
+        context_dict['law'] = law;
+    except Law.DoesNotExist:
+        pass
+
+    return render(request, 'representME/law.html', context_dict)
--- tangowithdjango/tangowithdjango/settings.py
+++ tangowithdjango/tangowithdjango/settings.py
@@ -97,3 +97,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+# TEMPLATES
+
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
+)
