b320af52063df5941d14289442ded2105ee081ba
jamesrferguson <jrfguitar24@gmail.com>
2015-03-08 08:27:24 +0000

    add number voted for & against and msps for logged in user

--- tangowithdjango/representME/views.py
+++ tangowithdjango/representME/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from representME.models import Law, Comment
+import re
+import requests
+import json
+from representME.models import Law, Comment, UserVote, Constituency, MSP
 from representME.forms import UserForm, UserProfileForm, LawCommentForm
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponse
@@ -26,17 +29,113 @@ def law(request, law_name):
         law = Law.objects.get(name=law_name)
         comments = Comment.objects.order_by('-time').filter(law=law)
 
-        context_dict['law'] = law;
+        votes = UserVote.objects.get(law=law)
+
+        context_dict['votes_for'] = votes_for(votes)
+        context_dict['votes_against'] = votes_against(votes)
+        context_dict['law'] = law
         context_dict['law_result'] = law_result(law)
         context_dict['commentForm'] = LawCommentForm()
         context_dict['comments'] = comments
         context_dict['comments_number'] = comments.count()
 
-
     except Law.DoesNotExist:
         pass
 
+    #check if user is logged in
+    if request.user.is_authenticated():
+        member_msps = get_msps(user.postcode)
+    else:
+        member_msps = {}
+
+    context_dict['member_msps'] = member_msps
+
     return render(request, 'representME/law.html', context_dict)
+
+#Reused from Cristina's previous project
+def is_valid(postcode):
+    """
+    check if parameter provided could be a postcode
+    :param postcode:
+    :return:True or False
+    """
+    postcode = postcode.upper()
+    postcode = re.sub('\s+', '', postcode)
+    inward = 'ABDEFGHJLNPQRSTUWXYZ'
+    fst = 'ABCDEFGHIJKLMNOPRSTUWYZ'
+    sec = 'ABCDEFGHJKLMNOPQRSTUVWXY'
+    thd = 'ABCDEFGHJKSTUW'
+    fth = 'ABEHMNPRVWXY'
+
+    return None != (re.match('[%s][1-9]\d[%s][%s]$' % (fst, inward, inward), postcode) or
+                    re.match('[%s][1-9]\d\d[%s][%s]$' % (fst, inward, inward), postcode) or
+                    re.match('[%s][%s]\d\d[%s][%s]$' % (fst, sec, inward, inward), postcode) or
+                    re.match('[%s][%s][1-9]\d\d[%s][%s]$' % (fst, sec, inward, inward), postcode) or
+                    re.match('[%s][1-9][%s]\d[%s][%s]$' % (fst, thd, inward, inward), postcode) or
+                    re.match('[%s][%s][1-9][%s]\d[%s][%s]$' % (fst, sec, fth, inward, inward), postcode))
+
+#Reused from Cristina's previous project
+def get_constituencies(postcode):
+    """
+    request regions mapit.mysociety.org for the given postcode
+    :param postcode:
+    :return:dictionary
+    """
+    if not is_valid(postcode):
+        return {'ERROR': "Invalid postcode."}
+
+    r = requests.get("http://mapit.mysociety.org/postcode/" + postcode)
+    if r.status_code != 200:
+        return {'ERROR': "There was an error looking up that postcode."}
+
+    data = json.loads(r.content)
+    areas = data['areas']
+    regions = {}
+    for item in areas:
+        if areas[item]['type_name'].startswith("Scottish Parliament constituency"):
+            regions['constituency'] = [str(areas[item]['name'])]
+        if areas[item]['type_name'].startswith("Scottish Parliament region"):
+            regions['region'] = [str(areas[item]['name'])]
+
+    return regions
+
+#Reused from Cristina's previous project
+def get_msps(postcode):
+    """
+    search the database for msps for each constituency received from get_constituencies
+    :param postcode:
+    :return:dictionary
+    """
+    regions = get_constituencies(postcode)
+    if regions.has_key('ERROR'):
+        return regions
+
+    print regions
+    msps = {}
+    for item in regions:
+        print regions[item][0]
+        const = Constituency.objects.filter(name=regions[item][0])
+        for c in const:
+            msps[regions[item][0]] = MSP.objects.filter(constituency=c.id)
+    return msps
+
+#function to return the number of votes for, takes a list of votes corresponding to a given law
+#and returns the number of votes in favour
+def votes_for(votes):
+    votes_for = 0
+    for vote in votes:
+        if vote.vote == "1":
+            votes_for += 1
+    return votes_for
+
+#function to return the number of votes against, takes a list of votes corresponding to a given laq
+#and returns the number of votes against it
+def votes_against(votes):
+    votes_against = 0
+    for vote in votes:
+        if vote.vote == "2":
+            votes_against += 1
+    return votes_against
 
 def law_result(law):
     if law.result == "1":
