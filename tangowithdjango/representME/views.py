54c5bbbc6d4cb42bd88fc475bae0761c1191aa32
Emil Cieslar <emil.cieslar@gmail.com>
2015-03-17 17:37:16 +0000

    Errors during logging in displayed nicely now

--- tangowithdjango/representME/views.py
+++ tangowithdjango/representME/views.py
@@ -293,11 +293,11 @@ def user_login(request):
                 return HttpResponseRedirect('/representME/')
             else:
                 # An inactive account was used - no logging in!
-                return HttpResponse("Your Rango account is disabled.")
+                return HttpResponseRedirect('/representME/#login-disabled')
         else:
             # Bad login details were provided. So we can't log the user in.
             print "Invalid login details: {0}, {1}".format(username, password)
-            return HttpResponse("Invalid login details supplied.")
+            return HttpResponseRedirect('/representME/#login-invalid')
 
     # The request is not a HTTP POST, so display the login form.
     # This scenario would most likely be a HTTP GET.
