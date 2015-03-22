4b688aaa0564edbf665fc7f35f558f957ca37415
Emil Cieslar <emil.cieslar@gmail.com>
2015-03-22 18:51:40 +0100

    Search work in progress

--- tangowithdjango/representME/views.py
+++ tangowithdjango/representME/views.py
@@ -310,7 +310,7 @@ def search(request):
                 if not law.topic in search_results_topics.keys():
                     search_results_topics[law.topic] = [entry]
                 elif not entry in search_results_topics[law.topic]:
-                    search_results_topics[topic].append(entry)
+                    search_results_topics[law.topic].append(entry)
             for msp in chain(msps.filter(firstname__icontains=term), msps.filter(lastname__icontains=term)):
                 if not msp in search_results_MSPs:
                     search_results_MSPs.append(msp)
