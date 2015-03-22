d5d578d2283c403a6999750c36b4f56219b42217
Emil Cieslar <emil.cieslar@gmail.com>
2015-03-22 17:29:56 +0100

    Search testing version

--- tangowithdjango/representME/views.py
+++ tangowithdjango/representME/views.py
@@ -304,10 +304,10 @@ def search(request):
         #retrieve results for each query term
         for term in query_terms:
             for topic in topics.filter(name__icontains=term):
-                search_results_topics[topic] = [[law, law.text[:200], get_time_tag(law)] for law in
+                search_results_topics[topic] = [[law, law.text[:300], get_time_tag(law)] for law in
                                                 laws.filter(topic=topic)]
             search_results_laws.extend(
-                [[law, law.text[:200], get_time_tag(law)] for law in laws.filter(text__icontains=term)])
+                [[law, law.text[:300], get_time_tag(law)] for law in laws.filter(text__icontains=term)])
             search_results_MSPs.extend(
                 list(chain(msps.filter(firstname__icontains=term), msps.filter(lastname__icontains=term))))
 
